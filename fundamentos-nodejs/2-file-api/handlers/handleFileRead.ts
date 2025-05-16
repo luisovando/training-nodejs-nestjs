eeb91c7860433419a21f679a3be174e9348910e4
Luis Ovando <isc.luisovando@gmail.com>
2025-05-15 19:59:25 -0600

    feat(file): complete GET /file implementation and tests
    Signed-off-by: Luis Ovando <isc.luisovando@gmail.com>

--- fundamentos-nodejs/2-file-api/handlers/handleFileRead.ts
+++ fundamentos-nodejs/2-file-api/handlers/handleFileRead.ts
@@ -1,5 +1,23 @@
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
 import { ServerResponse } from 'http';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
-export function handleFileRead(res: ServerResponse) {
-  return res.writeHead(404).end(JSON.stringify({ error: 'File not found' }));
+export async function handleFileRead(res: ServerResponse) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filePath = resolve(__dirname, '../data.json');
+  const fileExist = existsSync(filePath);
+
+  if (!fileExist) {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .writeHead(404)
+      .end(JSON.stringify({ error: 'File not found' }));
+  }
+
+  const content = await readFile(filePath, 'utf-8');
+  res.setHeader('Content-Type', 'application/json').writeHead(200).end(content);
 }
